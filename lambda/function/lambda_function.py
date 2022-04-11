79892fd3089f9cf0cd2d5c3e415d30887cb4eeaa
Issa Memari <issa.memari@gmail.com>
2022-04-11 18:47:37 +0200

    Write last_email only once in the zip

--- lambda/function/lambda_function.py
+++ lambda/function/lambda_function.py
@@ -59,19 +59,16 @@ def lambda_handler(event, context):
     else:
         logger.info("## NO EMAIL SENT")
 
-    with contextlib.suppress(OSError):
-        with open("last_email.txt", "w") as f:
-            f.write(email)
-    logger.info("## NEW EMAIL SAVED")
-
     bio = io.BytesIO()
     with zipfile.ZipFile(bio, "w") as zipf:
+        # Write Python files into the zip
+        for filename in os.listdir("."):
+            if filename != "last_email.txt":
+                zipf.write(filename)
+
         info = zipfile.ZipInfo("last_email.txt")
         info.external_attr = 0o644 << 16
         zipf.writestr(info, email)
-
-        for file in os.listdir("."):
-            zipf.write(file)
 
     client.update_function_code(
         FunctionName="monitor-tennis", ZipFile=bio.getvalue(),
